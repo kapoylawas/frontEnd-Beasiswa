0a4b39928c455f0c697b1686b5fb1d9960fc3357
arief sangga <arif.sangga@gmail.com>
2024-01-27 20:34:53 +0700

    update form luar negeri

--- src/views/Admin/Dispora/LuarNegeri.jsx
+++ src/views/Admin/Dispora/LuarNegeri.jsx
@@ -14,12 +14,16 @@ export default function LuarNegeri() {
   //navigata
   const navigate = useNavigate();
 
+  const [ipk, setIpk] = useState("");
+  const [transkrip, setTranskrip] = useState("");
+  const [isLoading, setLoading] = useState(false);
+
+
   //token from cookies
   const token = Cookies.get("token");
 
   const [users, setUsers] = useState("");
   const [step, setStep] = useState("");
-  console.log(step);
 
   const [errors, setErros] = useState([]);
 
@@ -52,6 +56,109 @@ export default function LuarNegeri() {
               <i className="fa-solid fa-backward"></i> Kembali
             </Link>
           </div>
+          {users === 1 ? (
+            <div className="alert alert-danger" role="alert">
+              Anda Sudah Terdaftar di Beasiswa
+            </div>
+          ) : step === 1 ? (
+            <div className="alert alert-danger" role="alert">
+              Anda Belum Menyelesaikan step 2 di Data Perguruan Tinggi Anda
+            </div>
+          ) : (
+            <div className="row">
+              <div className="col-md-12">
+                <div className="card border-0 rounded shadow-sm border-top-success">
+                  <div className="card-body">
+                    <h6>
+                      <i className="fa fa-shield-alt"></i> Beasiswa Disporapar
+                      Akademik
+                    </h6>
+                    <hr />
+                    <form>
+                      <div className="row">
+                        <div className="col-md-12">
+                          <div className="mb-3">
+                            <label className="form-label fw-bold">
+                              Indeks Prestasi Kumulatif (IPK) / Grade Point
+                              Average (GPA) / Yang dipersamakan
+                            </label>
+                            <input
+                              type="text"
+                              className="form-control"
+                              value={ipk}
+                              onChange={(e) => setIpk(e.target.value)}
+                              placeholder="Indeks Prestasi Kumulatif (IPK) / Grade Point Average (GPA) / Yang dipersamakan"
+                            />
+                          </div>
+                          {errors.ipk && (
+                            <div className="alert alert-danger">
+                              {errors.ipk[0]}
+                            </div>
+                          )}
+                        </div>
+                      </div>
+                      <div className="row">
+                        <div className="col-md-12">
+                          <div className="mb-3">
+                            <label className="form-label fw-bold">
+                              Transkip nilai IPK ? GPA pada semester terakhir
+                              yang sudah dijalani
+                            </label>
+                            <input
+                              type="file"
+                              className="form-control"
+                              value={transkrip}
+                              onChange={(e) => setTranskrip(e.target.value)}
+                            />
+                          </div>
+                          {errors.ipk && (
+                            <div className="alert alert-danger">
+                              {errors.ipk[0]}
+                            </div>
+                          )}
+                        </div>
+                      </div>
+                      <div className="row">
+                        <div className="col-md-12">
+                          <div className="mb-3">
+                            <label className="form-label fw-bold">
+                              Perguruan Tinggi yang diakui oleh Kementerian
+                              Pendidikan, Kebudayaan, Riset dan Teknologi Pada link berikut ini : (Upload Screenshoot)
+                              <br />
+                              <a target="_blank" href="https://ijazahln.kemdikbud.go.id/ijazahln/pencarian/pencarian-pt.html">https://ijazahln.kemdikbud.go.id/ijazahln/pencarian/pencarian-pt.html</a>
+                            </label>
+                            <input
+                              type="file"
+                              className="form-control"
+                              value={transkrip}
+                              onChange={(e) => setTranskrip(e.target.value)}
+                            />
+                          </div>
+                          {errors.ipk && (
+                            <div className="alert alert-danger">
+                              {errors.ipk[0]}
+                            </div>
+                          )}
+                        </div>
+                      </div>
+                      <div className="d-flex justify-content-center">
+                        <button
+                          type="submit"
+                          className="btn btn-md btn-primary me-2"
+                          disabled={isLoading}
+                        >
+                          {isLoading ? "LOADING..." : "SIMPAN"}{" "}
+                        </button>
+                        <button type="reset" className="btn btn-md btn-warning">
+                          <i className="fa fa-redo"></i> Reset
+                        </button>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </main>
     </LayoutAdmin>
