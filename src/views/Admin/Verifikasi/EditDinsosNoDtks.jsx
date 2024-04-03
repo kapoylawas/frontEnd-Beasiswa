99a6b960eb75ad2bd6ea63b19c082f79c0deac86
arief sangga <arif.sangga@gmail.com>
2024-04-03 21:58:16 +0700

    udate dinsos no dtks

--- src/views/Admin/Verifikasi/EditDinsosNoDtks.jsx
+++ src/views/Admin/Verifikasi/EditDinsosNoDtks.jsx
@@ -16,6 +16,7 @@ import Cookies from "js-cookie";
 //import toast
 import toast from "react-hot-toast";
 import Loading from "../../../components/general/Loading";
+import DinsosNoDtks from "../../../components/admin/DinsosNoDtks";
 
 export default function EditDinsosNoDtks() {
   //title page
@@ -34,6 +35,64 @@ export default function EditDinsosNoDtks() {
 
   const [isLoading, setLoading] = useState(false);
 
+  const [dataDinsos, setDataDinsos] = useState({
+    imagesktm: "",
+  });
+
+  const [dataUsers, setDataUsers] = useState({
+    name: "",
+    nik: "",
+    kartuKeluarga: "",
+    noHp: "",
+    email: "",
+    alamat: "",
+    rt: "",
+    rw: "",
+    imageKtp: "",
+    imageKartuKeluarga: "",
+    imageAktifkampus: "",
+    imageSuratpernyataan: "",
+    imageAkrekampus: "",
+    imageSuratBeasiswa: "",
+    imagesktm: "",
+  });
+
+  const [idUser, setIdUser] = useState("");
+  const [alasan, setAlasan] = useState("");
+  const [jenisVerif, setJenisVerif] = useState("");
+
+  //action handel jenis verif
+  const handleShowHideJenisVerif = (event) => {
+    const getType = event.target.value;
+    setJenisVerif(getType);
+  };
+
+  const fetchDataDinsos = async () => {
+    setLoading(true);
+    await Api.get(`/api/admin/dinsos/${id}`, {
+      headers: {
+        //header Bearer + Token
+        Authorization: `Bearer ${token}`,
+      },
+    }).then((response) => {
+      //set response data to state
+      setDataDinsos(response.data.data);
+      setDataUsers(response.data.data.user);
+      setIdUser(response.data.data.user.id);
+      setAlasan(response.data.data.user.alasan);
+      setJenisVerif(response.data.data.user.jenis_verif);
+      setTimeout(() => {
+        setLoading(false);
+      }, 500);
+    });
+  };
+
+  //useEffect
+  useEffect(() => {
+    //call function "fetchDataPost"
+    fetchDataDinsos();
+  }, []);
+
   return (
     <LayoutAdmin>
       <main>
@@ -47,6 +106,121 @@ export default function EditDinsosNoDtks() {
               >
                 <i className="fa fa-long-arrow-alt-left me-2"></i> Back
               </Link>
+              {isLoading ? (
+                <div className="mt-5">
+                  <Loading />
+                </div>
+              ) : (
+                <>
+                  <div className="row mt-1 mb-2">
+                    <div className="col-md-12">
+                      <div className="card border-0 rounded shadow-sm border-top-success">
+                        <div className="card-header text-dark">
+                          Verifikasi Data Beasiswa Non Akademik
+                        </div>
+                        <div className="card-body">
+                          <div className="table-responsive">
+                            <table className="table table-bordered table-striped text-dark">
+                              <tbody>
+                                <tr>
+                                  <td
+                                    style={{ width: "25%" }}
+                                    className="fw-bold text-center"
+                                  >
+                                    Isi Alasan dan Verifikasi
+                                  </td>
+                                  <td className="fw-bold text-center">
+                                    <form>
+                                      <div className="row">
+                                        <div className="col-md-12">
+                                          <label className="form-label fw-bold">
+                                            Isi Alasan
+                                          </label>
+                                          <textarea
+                                            rows="5"
+                                            cols="50"
+                                            value={alasan}
+                                            onChange={(e) =>
+                                              setAlasan(e.target.value)
+                                            }
+                                            className="form-control"
+                                          />
+                                        </div>
+                                        {errors.alasan && (
+                                          <div className="alert alert-danger">
+                                            {errors.alasan[0]}
+                                          </div>
+                                        )}
+                                      </div>
+                                      <div className="row mt-2">
+                                        <div className="col-md-12">
+                                          <div className="mb-3">
+                                            <label className="form-label fw-bold">
+                                              Pilih Verifikasi
+                                            </label>
+                                            <select
+                                              className="form-select"
+                                              value={jenisVerif}
+                                              onChange={
+                                                handleShowHideJenisVerif
+                                              }
+                                            >
+                                              <option value="">
+                                                -- Pilih Jenis Verifikasi --
+                                              </option>
+                                              <option value="lolos">
+                                                Lolos Verifikasi
+                                              </option>
+                                              <option value="tidak">
+                                                Tidak Lolos
+                                              </option>
+                                            </select>
+                                          </div>
+                                          {errors.jenis_verif && (
+                                            <div className="alert alert-danger">
+                                              {errors.jenis_verif[0]}
+                                            </div>
+                                          )}
+                                        </div>
+                                      </div>
+                                      <div className="d-flex justify-content-center">
+                                        <button
+                                          type="submit"
+                                          className="btn btn-md btn-primary me-2"
+                                          disabled={isLoading}
+                                        >
+                                          {isLoading ? "LOADING..." : "SIMPAN"}{" "}
+                                        </button>
+                                      </div>
+                                    </form>
+                                  </td>
+                                </tr>
+                              </tbody>
+                            </table>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <DinsosNoDtks
+                    name={dataUsers.name}
+                    nik={dataUsers.nik}
+                    kartuKeluarga={dataUsers.nokk}
+                    noHp={dataUsers.nohp}
+                    email={dataUsers.email}
+                    alamat={dataUsers.alamat}
+                    rt={dataUsers.rt}
+                    rw={dataUsers.rw}
+                    imageKtp={dataUsers.imagektp}
+                    imageKartuKeluarga={dataUsers.imagekk}
+                    imageAktifkampus={dataUsers.imageaktifkampus}
+                    imageSuratpernyataan={dataUsers.imagesuratpernyataan}
+                    imageAkrekampus={dataUsers.imageakrekampus}
+                    imageSuratBeasiswa={dataUsers.imagesuratbeasiswa}
+                    imagesktm={dataDinsos.imagesktm}
+                  />
+                </>
+              )}
             </div>
           </div>
         </div>
