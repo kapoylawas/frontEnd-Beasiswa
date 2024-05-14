bf20c4505a5b969e5667ab59247b587a2c6e6f16
arief sangga <arief.heaven@gmail.com>
2024-05-14 10:04:38 +0700

    update dasboard index

--- src/views/Admin/Index.jsx
+++ src/views/Admin/Index.jsx
@@ -700,7 +700,7 @@ export default function Dashboard() {
             </div>
           )}
 
-          {hasAnyPermission(["dispenduk.index"]) && (
+          {hasAnyPermission(["dashboard.index"]) && (
             <div className="row">
               <div className="col-xl-3 col-md-6">
                 <div className="card bg-primary text-white mb-4 border-0 shadow-sm">
