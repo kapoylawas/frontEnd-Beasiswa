4dd1265d356c5b638f3cb79d7d513be4955e607d
kapoylawas <arif.sangga@gmail.com>
2025-03-18 08:41:33 +0700

    update url API to env

--- src/services/Api.jsx
+++ src/services/Api.jsx
@@ -1,46 +1,42 @@
-//import axios
 import axios from "axios";
-
-//import js cookie
 import Cookies from "js-cookie";
 
 const Api = axios.create({
-  //set endpoint API
-  // baseURL: 'http://127.0.0.1:8000',
-  baseURL: "https://cdkbeasiswa.sidoarjokab.go.id",
+  // Set endpoint API from environment variable
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 
-  //set header axios
+  // Set header axios
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
   },
 });
 
-//handle unathenticated
+// Handle unauthenticated
 Api.interceptors.response.use(
   function (response) {
-    //return response
+    // Return response
     return response;
   },
   (error) => {
-    //check if response unauthenticated
+    // Check if response is unauthenticated
     if (401 === error.response.status) {
-      //remove token
+      // Remove token
       Cookies.remove("token");
 
-      //remove user
+      // Remove user
       Cookies.remove("user");
 
-      //remove permissions
+      // Remove permissions
       Cookies.remove("permissions");
 
-      //redirect "/"
+      // Redirect to "/"
       window.location = "/";
     } else if (403 === error.response.status) {
-      //redirect "/forbidden"
+      // Redirect to "/forbidden"
       window.location = "/forbidden";
     } else {
-      //reject promise error
+      // Reject promise error
       return Promise.reject(error);
     }
   }
