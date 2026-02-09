6bcf108a2a3fc69fc002e9861540ea60b7ef3a94
sangga <arif.sangga@gmail.com>
2026-02-09 16:48:25 +0700

    update semester

--- src/views/Admin/Dispora/Akademik.jsx
+++ src/views/Admin/Dispora/Akademik.jsx
@@ -267,7 +267,6 @@ export default function Akademik() {
                               onChange={handleshowhideSemester}
                             >
                               <option value="">-- Select Semester --</option>
-                              <option value="2">2</option>
                               <option value="3">3</option>
                               <option value="4">4</option>
                               <option value="5">5</option>
